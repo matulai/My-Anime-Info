44131f9c4f55e1ba32cb7a48440d057a0d8be2cb
Matias Laime <matulai1221@gmail.com>
2024-08-04 00:01:26 -0300

    pequeño cambio

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,22 +5,7 @@ const API_ANIME_URL = 'https://api.jikan.moe/v4';
 // faltan catchs y manejo de errores
 
 const getRandomAnime = () =>
-  axios.get(`${API_ANIME_URL}/random/anime`).then((response) => {
-    if (['TV', 'OVA', 'Movie'].includes(response.data.data.type)) {
-      return response.data;
-    } else {
-      return getRandomAnime();
-    }
-  });
-
-// async function getRandomAnimeAsync() {
-//   const response = await axios.get(`${API_ANIME_URL}/random/anime`);
-//   if (['TV', 'OVA', 'Movie'].includes(response.data.data.type)) {
-//     return response.data;
-//   } else {
-//     return getRandomAnimeAsync();
-//   }
-// }
+  axios.get(`${API_ANIME_URL}/random/anime`).then((response) => response.data);
 
 const getAnimeByName = (name) =>
   axios
