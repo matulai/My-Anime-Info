1e1911b8bf9398f53590760e561efda0d657ec68
Matias Laime <matulai1221@gmail.com>
2024-08-05 22:38:55 -0300

    creacion de navPagination, solo base.

--- src/components/AnimesBox/index.tsx
+++ src/components/AnimesBox/index.tsx
@@ -2,22 +2,22 @@ import Spinner from '../Spinner';
 import './AnimesBox.css';
 
 const AnimesBox: React.FC<AnimeArrProp> = ({ animeArr, isLoading }) => {
-  if (isLoading) {
-    return <Spinner />;
-  }
-
   return (
     <div className="animesbox-container">
-      {animeArr.map((anime) => (
-        <div className="animesbox-container-anime" key={anime.id}>
-          <img
-            className="animesbox-container-anime-image"
-            src={anime.imageUrl}
-            alt={anime.title}
-          />
-          <div className="animesbox-container-anime-title">{anime.title}</div>
-        </div>
-      ))}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        animeArr.map((anime) => (
+          <div className="animesbox-container-anime" key={anime.id}>
+            <img
+              className="animesbox-container-anime-image"
+              src={anime.imageUrl}
+              alt={anime.title}
+            />
+            <div className="animesbox-container-anime-title">{anime.title}</div>
+          </div>
+        ))
+      )}
     </div>
   );
 };
