4bacd7aa8f85925488d4fa152e8f48b40097bba2
Matias Laime <matulai1221@gmail.com>
2024-12-19 21:19:55 -0300

    Se agregan links home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <div className="header-container">
-      <h1 className="header-container-title">My Anime List</h1>
+      <Link to={`/`}>
+        <h1 className="header-container-title">My Anime List</h1>
+      </Link>
       <div className="header-container-buttons">
         <button className="header-button header-container-buttons-singUp">
           Sing Up
