64ceb62a63b7ab7b691dc6d5c56cd3cc9484eef8
Matias Laime <matulai1221@gmail.com>
2024-12-31 21:12:48 -0300

    Se crea componente button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import './Button.css';
+
+interface ButtonProps {
+  children?: React.ReactNode;
+  type: 'primary' | 'secondary' | 'onlyicon';
+  onClick?: () => void;
+  title?: string;
+}
+
+function Button({ children, type, onClick, title }: ButtonProps) {
+  const styles = {
+    primary: 'button-rectangle color-yellow',
+    secondary: 'button-rectangle color-white',
+    onlyicon: 'button-icon-no-background',
+  };
+
+  const className = styles[type] || '';
+
+  return (
+    <button className={className} onClick={onClick}>
+      {title}
+      {children}
+    </button>
+  );
+}
+
+export default Button;
