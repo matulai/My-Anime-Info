b2f3ff2bc7c27a45dce6c75f020a7bb89eda5246
Matias Laime <matulai1221@gmail.com>
2025-02-05 22:39:52 -0300

    Se cambia extension de imagen notfound a webp

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -4,7 +4,7 @@ const NotFoundPage = () => {
   return (
     <div className="not-found-page-container">
       <h1>This page doesn't exist</h1>
-      <img src="/src/assets/NotFoundImage.png" alt="404 Not Found" />
+      <img src="/src/assets/NotFoundImage.webp" alt="404 Not Found" />
     </div>
   );
 };
