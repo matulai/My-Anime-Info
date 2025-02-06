d183995c3486f4e0f4beda60d28237fe3e5694a2
Matias Laime <matulai1221@gmail.com>
2025-02-05 22:39:22 -0300

    Se agrega logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,12 @@
 import { Link } from 'react-router-dom';
-import Button from '@/components/Button';
 import './Header.css';
 
 const Header = () => {
   return (
     <div className="header-container">
       <Link to={`/`}>
-        <h1 className="header-container-title">My Anime List</h1>
+        <h1 className="header-container-title">My Anime Info</h1>
       </Link>
-      <div className="header-container-buttons">
-        <Button type="primary" title="Sing Up" />
-        <Button type="secondary" title="Log In" />
-      </div>
     </div>
   );
 };
