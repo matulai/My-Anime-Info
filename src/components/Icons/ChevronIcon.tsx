5a9f3d4ef1635d88d73b907de4f889707d0f9423
Matias Laime <matulai1221@gmail.com>
2025-09-24 00:51:15 -0300

    Add: chevron icon

---
+++ src/components/Icons/ChevronIcon.tsx
@@ -0,0 +1,22 @@
+const ChevronIcon = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M30 12L18 24L30 36"
+        stroke="#ffcc00"
+        strokeOpacity="0.9"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ChevronIcon;
