26bd62d8593b967163b36b0d4cad689be409cab4
Matias Laime <matulai1221@gmail.com>
2025-10-06 01:38:47 -0300

    Fix: nav style on link active for dinamic routes

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,28 +1,35 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const location = useLocation();
+
   const navLinks = [
     { to: '/', label: 'Animes' },
-    { to: '/top/anime/1', label: 'Top Animes' },
-    { to: '/seasons', label: 'Seasonal Animes' },
+    { to: '/top/anime/1', label: 'Top Animes', matchPrefix: '/top/anime' },
+    { to: '/seasons', label: 'Seasonal Animes', matchPrefix: '/seasons' },
     { to: '/random/animes', label: 'Random' },
   ];
 
   return (
     <div className="navbar-container">
-      {navLinks.map((link, index) => (
-        <NavLink
-          key={index}
-          to={link.to}
-          end={link.to == '/'}
-          className={({ isActive }) =>
-            isActive ? 'navbar-container-nav active' : 'navbar-container-nav'
-          }
-        >
-          {link.label}
-        </NavLink>
-      ))}
+      {navLinks.map((link, index) => {
+        const isActive = link.matchPrefix
+          ? location.pathname.startsWith(link.matchPrefix)
+          : location.pathname === link.to;
+        console.log(isActive, link.to, location);
+        return (
+          <NavLink
+            key={index}
+            to={link.to}
+            className={
+              isActive ? 'navbar-container-nav active' : 'navbar-container-nav'
+            }
+          >
+            {link.label}
+          </NavLink>
+        );
+      })}
     </div>
   );
 };
